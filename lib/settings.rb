bdd4f1b41fe7cd507d4868163c7c7bc499db8fc9
Maciej Gajek <maciej@qype.com>
2011-10-20 00:31:19 +0200

    better auth

--- lib/settings.rb
+++ lib/settings.rb
@@ -10,10 +10,6 @@ class Settings
       "http://apps.facebook.com/karny-kutas/"
     end
 
-    def auth_url
-      "https://www.facebook.com/dialog/oauth?client_id=#{app_id}&redirect_uri=#{CGI::escape(app_url)}&scope=publish_stream"
-    end
-
   end
 
 end
